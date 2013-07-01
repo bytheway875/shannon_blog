2344cc07d8ceb055b401e4f44fbcf8c2446769b1
MakerSquare Student <student@themakersquare.com>
2013-07-01 15:18:05 -0500

    Created methods for posting blogs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,17 @@
 class PostsController < ApplicationController
 
 	def index
+		@name = "Shannon"
+		@posts = Post.all
 	end
+
+	def create
+		Post.create(person_params)
+	end
+
+	end
+
+	private
+		def post_params
+			params.require(:title, :body);
 end
