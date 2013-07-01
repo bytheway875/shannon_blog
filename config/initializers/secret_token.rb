586f68e6637a67139da0b9615e93549a5b2905b3
MakerSquare Student <student@themakersquare.com>
2013-07-01 13:57:53 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShannonBlog::Application.config.secret_key_base = '0951c4fe47804f5c52f541a70420d5d2b5bd8a01b6ebf0d043bbcf7d7da2e035404f8044c62d74b1913b3c66ab8a3345e5a4bf9c4ad138133a9632fa8be078ce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShannonBlog::Application.config.session_store :cookie_store, key: '_shannon_blog_session'
